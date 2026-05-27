3511a114e7af78b9be9b1917b3749facc0e5ca5a
bhagwatSparsh <sparshbhagwat18@gmail.com>
2026-05-27 23:32:15 +0530

    1. Two Sum

---
+++ 16_Two Sum(leetcode).cpp
@@ -0,0 +1,59 @@
+//Better solution using hash map
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        map<int,int> mpp;
+        for (int i=0;i<n;i++){
+            int a=nums[i];
+            int req=target-a;
+            if(mpp.find(req)!=mpp.end()){ // if the elements exists in hash map
+                return {mpp[req],i};
+            }
+            mpp[a]=i;
+        }
+        return {-1,-1};
+        
+    }
+};
+
+//Optimal solution using two pointer approach:
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        
+        vector<pair<int,int>> arr;
+
+        for(int i=0;i<n;i++){
+            arr.push_back({nums[i], i});
+        }
+
+        
+        sort(arr.begin(), arr.end());
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right){
+
+            int sum = arr[left].first + arr[right].first;
+
+            if(sum == target){
+                return {arr[left].second, arr[right].second};
+            }
+            else if(sum < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
+        return {-1,-1};
+    }
+};
