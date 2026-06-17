ea41584f028b32a2f6214350b40c2c8dcc766269
bhagwatSparsh <sparshbhagwat18@gmail.com>
2026-06-17 16:05:47 +0530

    1539. Kth Missing Positive Number

---
+++ 03_Binary_Search/52_Kth_missing_positive_no(leetcode).cpp
@@ -0,0 +1,38 @@
+// brute force approach:
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        for (int i=0;i<n;i++){
+            if (arr[i]<=k){
+                k++;
+            }
+        }
+        return k;
+        
+    }
+};
+
+// optimal solution:
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int missing=arr[mid]-(mid+1);
+            if (missing<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+
+            }
+        }
+        return low+k;
+        
+    }
+};
