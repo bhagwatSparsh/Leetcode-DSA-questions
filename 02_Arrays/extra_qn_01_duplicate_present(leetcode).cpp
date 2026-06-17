e731991c7321858d1bf444e88863d4e9a2f2f92c
bhagwatSparsh <sparshbhagwat18@gmail.com>
2026-06-17 17:15:44 +0530

    217. Contains Duplicate

---
+++ 02_Arrays/extra_qn_01_duplicate_present(leetcode).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_set<int> mpp;
+        for (int i=0;i<n;i++){
+            if (mpp.count(nums[i])){
+                return true;
+            }
+            mpp.insert(nums[i]);
+
+        }
+        return false;
+
+        
+    }
+};
