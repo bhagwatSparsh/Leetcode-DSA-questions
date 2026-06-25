6c88970eb0731c0f8f4ffd84c491b1c360cb31e9
bhagwatSparsh <sparshbhagwat18@gmail.com>
2026-06-25 23:47:36 +0530

    13. Roman to Integer

---
+++ 04_Strings/71_Roman_to_integer(leetcode).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n=s.size();
+        map<char,int> mpp={
+           { 'I' , 1}, {'V',5},{'X',10},{
+            'L',50}, {'C',100},{'D', 500},{
+                'M', 1000}
+            
+           
+        };
+        int ans=0;
+
+        
+        for (int i=0;i<n;i++){
+            if (i+1<n && mpp[s[i]]<mpp[s[i+1]]){
+                ans+=mpp[s[i+1]]-mpp[s[i]];
+                i++;
+
+            }
+            else{
+                ans+=mpp[s[i]];
+            }
+        }
+        return ans;
+        
+    }
+};
