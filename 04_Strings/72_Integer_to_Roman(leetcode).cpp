77fef7f381e2143d432a734a096d1a0258dcc9d0
bhagwatSparsh <sparshbhagwat18@gmail.com>
2026-06-26 00:11:02 +0530

    12. Integer to Roman

---
+++ 04_Strings/72_Integer_to_Roman(leetcode).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        vector<pair<int, string>> roman={
+            {1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},
+            {50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}
+        };
+        int n=roman.size();
+        string ans="";
+
+        for (int i=0;i<n;i++){
+            while(num>=roman[i].first){
+                ans+=roman[i].second;
+                num=num-roman[i].first;
+
+            }
+
+        }
+        return ans;
+        
+    }
+};
